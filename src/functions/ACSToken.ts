97e7d1ff05d3c2a7c1b4791c4669746030a95584
Ruslan Zdor <ruslanzdor@microsoft.com>
2023-12-13 14:59:05 -0800

    rename function

--- src/functions/ACSToken.ts
+++ src/functions/ACSToken.ts
@@ -3,24 +3,19 @@ import { CommunicationIdentityClient } from "@azure/communication-identity";
 
 let client: CommunicationIdentityClient;
 
-export async function FirstFunction(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    const startTime = Date.now();
-
+export async function AcsToken(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     if (!client) await warmupFunction({} as WarmupContext, context);
-
     const user = await client.createUser();
-
-    const token = client.getToken(user, ["voip", "chat"]);
-
-    const name = user.communicationUserId || " " || (await token).token || ' world';
-
-    return { body: `${name}! 'it is took ' ${(Date.now() - startTime).toString()}`, status: 200};
+    const token = await client.getToken(user, ["voip", "chat"]);
+    const response = {
+        userId: user.communicationUserId,
+        token: token.token,
+        expiresOn: token.expiresOn
+    };
+    return { body: JSON.stringify(response), status: 200};
 };
 
 export async function warmupFunction(warmupContext: WarmupContext, context: InvocationContext): Promise<void> {
-    context.log('Function App instance is warm.');
     client = new CommunicationIdentityClient(process.env["connectionString"]);
 }
 
@@ -28,8 +23,8 @@ app.warmup('warmup', {
     handler: warmupFunction,
 });
 
-app.http('FirstFunction', {
+app.http('AcsToken', {
     methods: ['GET'],
     authLevel: 'anonymous',
-    handler: FirstFunction
+    handler: AcsToken
 });
